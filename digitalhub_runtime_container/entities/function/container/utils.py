9683468eb6feae594ad53c7608908a3ec8d2ca5e
EvangMM <matteomartini92@yahoo.it>
2025-01-27 11:38:11 +0100

    chore: small code reorganization

--- digitalhub_runtime_container/entities/function/container/utils.py
+++ digitalhub_runtime_container/entities/function/container/utils.py
@@ -4,10 +4,10 @@ import typing
 from pathlib import Path
 
 from digitalhub.stores.api import get_store
+from digitalhub.stores.s3.utils import get_s3_bucket_from_env
 from digitalhub.utils.exceptions import EntityError
 from digitalhub.utils.file_utils import eval_py_type, eval_text_type, eval_zip_type
 from digitalhub.utils.generic_utils import encode_source, encode_string
-from digitalhub.utils.s3_utils import get_s3_bucket
 from digitalhub.utils.uri_utils import has_local_scheme
 
 if typing.TYPE_CHECKING:
@@ -134,7 +134,7 @@ def source_post_check(exec: FunctionContainer) -> FunctionContainer:
         # Check zip
         elif eval_zip_type(code_src):
             filename = Path(code_src).name
-            dst = f"zip+s3://{get_s3_bucket()}/{exec.project}/{exec.ENTITY_TYPE}/{exec.name}/{exec.id}/{filename}"
+            dst = f"zip+s3://{get_s3_bucket_from_env()}/{exec.project}/{exec.ENTITY_TYPE}/{exec.name}/{exec.id}/{filename}"
             get_store(exec.project, dst).upload(code_src, dst)
             exec.spec.source["source"] = dst
             if exec.spec.source.get("handler") is None:
