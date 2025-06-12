20bb96a0b38cb237b14e4912d142b2aa867ac4b9
EvangMM <matteomartini92@yahoo.it>
2025-06-12 09:54:07 +0200

    docs: added copyright header

--- digitalhub_runtime_container/__init__.py
+++ digitalhub_runtime_container/__init__.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: © 2025 DSLab - Fondazione Bruno Kessler
+#
 # SPDX-License-Identifier: Apache-2.0
 from digitalhub_runtime_container.entities._commons.enums import EntityKinds
 from digitalhub_runtime_container.entities.function.container.builder import FunctionContainerBuilder
